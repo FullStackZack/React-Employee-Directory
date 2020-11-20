a79ee312053bcafce7890ec2164875fbfc325626
Zack White <zhwhite18@yahoo.com>
2020-11-19 18:50:00 -0600

    searchbar functional

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import Navbar from "./components/Navbar";
 import Wrapper from "./components/Wrapper";
 import Main from "./components/Main";
-import Footer from "./components/Footer";
 
 
 function App() {
 
   return (
     <div>
-      <Navbar />
       <Wrapper>
         <Main />
       </Wrapper>
-      <Footer />
     </div>
   )
 
